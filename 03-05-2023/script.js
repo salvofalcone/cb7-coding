3cc5cf80c135ea9e1624b42bff3eedf5c0bf61a5
Salvo Falcone <70477447+salvofalcone@users.noreply.github.com>
2023-05-03 13:40:08 +0200

    Lezione 3 maggio 2023

---
+++ 03-05-2023/script.js
@@ -0,0 +1,77 @@
+// da attivare per esercizio 1 e 2
+// let numb = parseInt(
+//   prompt("Inserisci un numero intero e ti dirò se è pari o dispari")
+// );
+
+// ESERCIZIO 1
+// if (numb % 2 === 0) {
+//   console.log("Hai inserito il numero ", numb, "ed è un numero pari");
+// } else {
+//   console.log("Hai inserito il numero ", numb, "ed è un numero dispari");
+// }
+
+// ESERCIZIO 2
+// numb%2 === 0
+//   ? console.log("Hai inserito il numero ", numb, "ed è un numero pari")
+//   : console.log("Hai inserito il numero ", numb, "ed è un numero dispari");
+
+// -------------------------------------------------------------------------
+
+// AVANZATO
+// let firstNumb = parseInt(prompt("Inserisci il primo numero:"));
+// let secondNumb = parseInt(prompt("Inserisci il secondo numero:"));
+// let result;
+// let operationChoice = prompt("Che operazione vuoi eseguire?");
+
+// switch (operationChoice) {
+//   case "+":
+//     result = firstNumb + secondNumb;
+//     console.log(
+//       "Hai scelto la somma! Ecco il tuo risultato",
+//       firstNumb,
+//       "+",
+//       secondNumb,
+//       "=",
+//       result
+//     );
+//     break;
+
+//   case "-":
+//     result = firstNumb - secondNumb;
+//     console.log(
+//       "Hai scelto la differenza! Ecco il tuo risultato",
+//       firstNumb,
+//       "-",
+//       secondNumb,
+//       "=",
+//       result
+//     );
+//     break;
+
+//   case "/":
+//     result = firstNumb / secondNumb;
+//     console.log(
+//       "Hai scelto la divisione! Ecco il tuo risultato",
+//       firstNumb,
+//       "/",
+//       secondNumb,
+//       "=",
+//       result
+//     );
+//     break;
+
+//   case "*":
+//     result = firstNumb * secondNumb;
+//     console.log(
+//       "Hai scelto la moltiplicazione! Ecco il tuo risultato",
+//       firstNumb,
+//       "*",
+//       secondNumb,
+//       "=",
+//       result
+//     );
+//     break;
+
+//   default:
+//     console.log("Qualcosa non va, ricominciamo?");
+// }
