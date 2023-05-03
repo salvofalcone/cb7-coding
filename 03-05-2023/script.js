2fd6d847e252ac2b60474b818cb2a34e6fcc1d00
Salvo Falcone <salvofalcone57@gmail.com>
2023-05-03 14:58:12 +0200

    Lezione 3 maggio 2023

--- 03-05-2023/script.js
+++ 03-05-2023/script.js
@@ -1,30 +1,36 @@
-// da attivare per esercizio 1 e 2
+// -------- ESERCIZIO 1 --------
+
 // let numb = parseInt(
 //   prompt("Inserisci un numero intero e ti dirò se è pari o dispari")
 // );
-
-// ESERCIZIO 1
 // if (numb % 2 === 0) {
 //   console.log("Hai inserito il numero ", numb, "ed è un numero pari");
 // } else {
 //   console.log("Hai inserito il numero ", numb, "ed è un numero dispari");
 // }
 
-// ESERCIZIO 2
-// numb%2 === 0
+// -------------------------------------------------------------------------
+
+// -------- ESERCIZIO 2 --------
+
+// let numb = parseInt(
+//   prompt("Inserisci un numero intero e ti dirò se è pari o dispari")
+// );
+// numb % 2 === 0
 //   ? console.log("Hai inserito il numero ", numb, "ed è un numero pari")
 //   : console.log("Hai inserito il numero ", numb, "ed è un numero dispari");
 
 // -------------------------------------------------------------------------
 
-// AVANZATO
+// -------- AVANZATO --------
+
 // let firstNumb = parseInt(prompt("Inserisci il primo numero:"));
 // let secondNumb = parseInt(prompt("Inserisci il secondo numero:"));
 // let result;
 // let operationChoice = prompt("Che operazione vuoi eseguire?");
 
 // switch (operationChoice) {
-//   case ("+", "addizione"):
+//   case ("+"):
 //     result = firstNumb + secondNumb;
 //     console.log(
 //       "Hai scelto la somma! Ecco il tuo risultato",
@@ -36,7 +42,7 @@
 //     );
 //     break;
 
-//   case ("-", "sottrazione", "differenza"):
+//   case ("-"):
 //     result = firstNumb - secondNumb;
 //     console.log(
 //       "Hai scelto la differenza! Ecco il tuo risultato",
@@ -48,7 +54,7 @@
 //     );
 //     break;
 
-//   case ("/", "divisione", "rapporto", "quoziente"):
+//   case ("/"):
 //     result = firstNumb / secondNumb;
 //     console.log(
 //       "Hai scelto la divisione! Ecco il tuo risultato",
@@ -60,7 +66,7 @@
 //     );
 //     break;
 
-//   case ("*", "moltiplicazione", "prodotto"):
+//   case ("*"):
 //     result = firstNumb * secondNumb;
 //     console.log(
 //       "Hai scelto la moltiplicazione! Ecco il tuo risultato",
