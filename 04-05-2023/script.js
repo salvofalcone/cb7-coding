983e5febc5ca4c990402b4043184f9fac7b9d406
Salvo Falcone <salvofalcone57@gmail.com>
2023-05-04 16:45:44 +0200

    Lezione 4 maggio 2023

--- 04-05-2023/script.js
+++ 04-05-2023/script.js
@@ -1,24 +1,43 @@
 // ------- ESERCIZIO 1 -------
 
-// let arrayInit = [
-//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-// ];
-// let arrayFinal = [];
+let arrayInit = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+let arrayFinal = [];
 
-// console.log("Array iniziale", arrayInit);
-// console.log("Array Finale", arrayFinal);
+console.log("Array iniziale", arrayInit);
+console.log("Array Finale", arrayFinal);
 
-// for (let i = arrayInit.length; i > 0; i--) {
-//   let x = arrayInit.pop();
-//   arrayFinal.push(x);
-// }
-// console.log("Array iniziale", arrayInit);
-// console.log("Array Finale", arrayFinal);
+for (let i = arrayInit.length; i > 0; i--) {
+  let x = arrayInit.pop();
+  arrayFinal.push(x);
+}
+console.log("Array iniziale", arrayInit);
+console.log("Array Finale", arrayFinal);
 
 // -------------------------------------------------
 
 // ------- ESERCIZIO 2 -------
+// let array = [-10390, 9, 10, 81, -10, 12, 0, -1, -9];
 
+// let max = array[0];
+// for (let item of array) {
+//   if (max < item) {
+//     max = item;
+//   }
+// }
+
+// let min = array[0];
+// for (let item of array) {
+//   if (min > item) {
+//     min = item;
+//   }
+// }
+
+// console.log("Il più piccolo è: ", +min);
+// console.log("Il più grande è: ", +max);
+
+// -------------------------------------------------
 // // nel caso in cui l'array contenga solo numeri compresi tra 0 a 9 (estremi inclusi)
 // let array = [6, 7, 8, 9, 1, 2, 3, 4, 5, 0];
 // let orderedArray = array.sort();
@@ -26,33 +45,3 @@
 
 // let inverseOrderedArray = orderedArray.reverse();
 // console.log("Il numero più grande è: ", +inverseOrderedArray[0]);
-
-// // nel caso in cui l'array contenga numeri maggiori a 9
-
-// let array = [9, 10, 81, ,-10, 12, 0, -1, -9];
-// let min = array.shift();
-// for (let item of array) {
-//   if (min > item) {
-//     min = item;
-//   }
-// }
-// console.log(min);
-
-let array = [-10390, 9, 10, 81, -10, 12, 0, -1, -9];
-
-let max = array[0];
-for (let item of array) {
-  if (max < item) {
-    max = item;
-  }
-}
-
-let min=array[0];
-for (let item of array) {
-  if (min > item) {
-    min = item;
-  }
-}
-
-console.log("Il più piccolo è: ",+min);
-console.log("Il più grande è: ",+max);
