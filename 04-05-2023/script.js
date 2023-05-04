6d608351695dded96de639d522bbca2ad176e18a
Salvo Falcone <salvofalcone57@gmail.com>
2023-05-04 14:22:28 +0200

    Lezione 04 maggio 2023

---
+++ 04-05-2023/script.js
@@ -0,0 +1,21 @@
+// // ESERCIZIO 1
+// let arrayInit = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+// let arrayFinal = [];
+
+// console.log("Array iniziale", arrayInit);
+// console.log("Array Finale", arrayFinal);
+
+// for (let i = arrayInit.length; i > 0; i--) {
+//   let x = arrayInit.pop();
+//   arrayFinal.push(x);
+// }
+// console.log("Array iniziale", arrayInit);
+// console.log("Array Finale", arrayFinal);
+
+// -------------------------------------------------
+
+// ESERCIZIO 2
+let array = [2, 77, 47, 100092];
+
